9e429f19d1c0fe5a311d164d4b6ec3732b662d33
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-04 10:21:31 +0800

    update

---
+++ 1.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end((new Date()).toString())
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
